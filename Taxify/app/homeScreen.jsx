26e9fd6ad901d789f86c5ffa472d2ad2b352b437
Rohit kumar <rohitprj.1282@gmail.com>
2025-03-02 16:07:37 +0530

    Remove unnecessary flex properties from HomeScreen for cleaner layout

--- Taxify/app/homeScreen.jsx
+++ Taxify/app/homeScreen.jsx
@@ -9,9 +9,6 @@ export default function HomeScreen() {
   return (
     <View
       style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
         backgroundColor: theme.background,
       }}
     >
